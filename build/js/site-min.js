ba36a2d0477849c424bbbaa40a6ffd01bc274721
Casey Friday <casey@friday-next.com>
2020-03-29 12:13:38 -0500

    Finished Carousel Hero for Desktop

---
+++ build/js/site-min.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){var e=$("#hero-slider-1").height();$("#hero-slider-2 > div").height(e)}));
+//# sourceMappingURL=site-min.js.map
---
+++ src/js/site.js
@@ -0,0 +1,4 @@
+$(document).ready( function () {
+    var slider_height = $('#hero-slider-1').height();
+    $('#hero-slider-2 > div').height(slider_height);
+});
