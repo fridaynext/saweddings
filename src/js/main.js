9b77606c1a8deec93fff25c77539b2b7fa17ceb2
Casey Friday <casey@friday-next.com>
2020-04-05 17:36:10 -0500

    Wrote jQuery/Javascript to ensure the homepage hero slider slides always remain the same height.

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,27 @@
-$(document).ready( function () {
-    var slider_height = $('#hero-slider-1').height();
-    $('#hero-slider-2 > div').height(slider_height);
+$(document).ready(function () {
+    // Set height of second slide, when carousel changes
+    $('#heroCarousel').on('slide.bs.carousel', function (event) {
+
+        let index = event.from;
+        let thisSlideHeight = $(this).css("height");
+        $(this).children().each(function (index) {
+            if (index < 1) {
+                $(this).children().each(function (indexB) {
+                    if (indexB < 1) {
+                        $(this).children().each(function (indexC) {
+                            thisSlideHeight = $(this).css("height");
+                            $(this).css("height", thisSlideHeight);
+                        });
+                    }
+                });
+            }
+        });
+
+        let nextSlideId = "#" + event.relatedTarget.firstElementChild.id;
+        $(nextSlideId).children().each(function (index) {
+            if (index < 1) {
+                $(this).css("height", thisSlideHeight);
+            }
+        });
+    })
 });
